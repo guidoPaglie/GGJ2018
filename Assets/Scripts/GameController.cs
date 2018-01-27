baac9d7080591fed07a864b37bad36006240a597
Guido Pagliettini <guido_pagliettini@hotmail.com>
2018-01-27 12:17:28 -0300

    Loading complete users .json

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour {
+
+	public GameObject PhoneUser;
+
+    private PhoneUsersLoader phoneUserLoader;
+
+	void Start () 
+    {
+        phoneUserLoader = new PhoneUsersLoader();	
+	}
+	
+	void Update () 
+    {
+		
+	}
+}
---
+++ Assets/Scripts/PhoneUser.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PhoneUser
+{
+	public string _name;
+	public int _id;
+
+	public Dictionary<int, JSONObject> roundTexts;
+
+	public PhoneUser(string name, int id)
+	{
+		_name = name;
+		_id = id;
+
+		roundTexts = new Dictionary<int, JSONObject>();
+	}
+
+	public void SetRoundJSON(int i, JSONObject jSONObject)
+	{
+		roundTexts[i] = jSONObject;
+	}
+}
