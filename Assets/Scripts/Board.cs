830f164ebc39951e4946702ae58d0361fbb95ac8
Guido Pagliettini <guidopagliettini@Guidos-MacBook-Pro.local>
2018-01-26 22:02:39 -0300

    project

---
+++ Assets/Scripts/Board.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Board : MonoBehaviour {
+
+	public GameObject Connector;
+
+	public int Rows;
+	public int Cols;
+
+	private RectTransform myRT;
+	private GameObject[,] board;
+
+	void Start () 
+	{
+		board = new GameObject[Rows, Cols];
+
+		for (int i = 0; i < Rows; i++) 
+		{
+			for (int j = 0; j < Cols; j++) 
+			{
+				GameObject obj = Instantiate (Connector, Vector3.zero, Quaternion.identity, this.transform);
+				board [i, j] = obj;
+			}
+		}
+	}
+	
+	void Update ()
+	{
+		
+	}
+}
