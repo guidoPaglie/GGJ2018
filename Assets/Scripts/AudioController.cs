d437e4988ffadc6b06165f367c8f48fac3e9b6be
Guido Pagliettini <guido_pagliettini@hotmail.com>
2018-01-27 01:45:13 -0300

    Main Menu and Gameplay Screens

---
+++ Assets/Scripts/AudioController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AudioController : MonoBehaviour {
+
+    public Button SoundBtn;
+    public Button MusicBtn;
+
+    public Image SoundImg;
+    public Sprite SoundOff;
+    public Sprite SoundOn;
+
+    public Image MusicImg;
+    public Sprite MusicOff;
+    public Sprite MusicOn;
+
+    private bool _soundOn;
+    private bool _musicOn;
+
+	void Start () {
+        _soundOn = PlayerPrefs.GetInt("sound", 0) == 1;
+        _musicOn = PlayerPrefs.GetInt("music", 0) == 1;
+
+        SoundBtn.onClick.AddListener(SwitchSoundState);
+        MusicBtn.onClick.AddListener(SwitchMusicState);
+	}
+
+    private void SwitchSoundState()
+    {
+        _soundOn = !_soundOn;
+        SoundImg.sprite = _soundOn ? SoundOn : SoundOff;
+
+        PlayerPrefs.SetInt("sound", Convert.ToInt32(_soundOn));
+    }
+
+    private void SwitchMusicState()
+    {
+        _musicOn = !_musicOn;
+        MusicImg.sprite = _musicOn ? MusicOn : MusicOff;
+
+        PlayerPrefs.SetInt("music", Convert.ToInt32(_musicOn));
+    }
+}
---
+++ Assets/Scripts/GameplayScreen/GameplayScreen.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class GameplayScreen : MonoBehaviour
+{
+    public Button BackBtn;
+
+    void Start()
+    {
+        BackBtn.onClick.AddListener(GoToMainMenu);
+    }
+
+    void GoToMainMenu()
+    {
+        SceneManager.LoadScene("MainMenu", LoadSceneMode.Single);
+    }
+}
