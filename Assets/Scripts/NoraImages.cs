fae537b8f9a12000ca30060663caa5e7a7af102a
Guido Pagliettini <guido_pagliettini@hotmail.com>
2018-01-28 14:01:45 -0300

    nora images

---
+++ Assets/Scripts/NoraImages.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NoraImages : MonoBehaviour {
+
+    public List<Sprite> noraSprites;
+    public SpriteRenderer noraSR;
+
+    private int lastRound = 0;
+
+	void Update () 
+    {
+        if (GameController._currentRound != lastRound)
+        {
+            if (lastRound < noraSprites.Count)
+            {
+                noraSR.sprite = noraSprites[lastRound];
+                lastRound++;    
+            }
+        }
+	}
+}
