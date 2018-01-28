d9fb11f42006dcef0bc78d8897e45bed1c374222
Guido Pagliettini <guido_pagliettini@hotmail.com>
2018-01-28 13:18:40 -0300

    game over screen

---
+++ Assets/Scripts/GameoverScreen.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameoverScreen : MonoBehaviour {
+
+    public List<GameObject> screens;
+    public AudioSource audioSource;
+
+    public AudioClip gameoverClip;
+
+    public float[] timers;
+    private int currentScreen = 0;
+
+	void Start () {
+        audioSource.Play();
+
+        StartCoroutine(StartAnimation());		
+	}
+
+    private IEnumerator StartAnimation()
+    {
+        while(currentScreen < 2)
+        {
+            yield return new WaitForSeconds(timers[currentScreen]);
+            screens[currentScreen].SetActive(false);
+            currentScreen++;
+            screens[currentScreen].SetActive(true);
+
+            if (currentScreen == 1)
+            {
+                audioSource.clip = gameoverClip;
+                audioSource.Play();
+            }
+        }
+    }
+}
